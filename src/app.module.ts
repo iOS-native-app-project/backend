da017fc7b5c4bed33f49bb8bfd64dd6518cebeea
zsunkim <jisun124918@gmail.com>
2022-02-19 15:38:01 +0900

    update AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KakaoModule } from './user/login/kakao/kakao.module';
+import { NaverService } from './user/login/naver/naver.service';
+import { NaverController } from './user/login/naver/naver.controller';
+import { NaverModule } from './user/login/naver/naver.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [KakaoModule, NaverModule],
+  controllers: [AppController, NaverController],
+  providers: [AppService, NaverService],
 })
-export class AppModule {}
+export class AppModule { }
