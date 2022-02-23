52d246ec1ddddb1a0df046283df0cf75886ff216
hwansu-k <hs@kumas.dev>
2022-02-23 13:15:56 +0900

    change from dotenv to nestconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
 import { KakaoModule } from './user/login/kakao/kakao.module';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { User } from './entities/User';
+import Joi from 'joi';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), KakaoModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        PORT: Joi.number().required(),
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.string().required(),
+        DATABASE_USERNAME: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [User],
+      charset: 'utf8mb4',
+      synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
+      logging: process.env.TYPEORM_LOGGING === 'true',
+    }),
+    KakaoModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { env } from './configs/env';
 
 declare const module: any;
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.app.port);
+  await app.listen(process.env.PORT);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
 
-  logger.log(`http Server is started on port ${env.app.port}`);
+  logger.log(`http Server is started on port ${process.env.PORT}`);
 }
 bootstrap();
