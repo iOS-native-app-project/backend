172a9fad95c366727ae8d09cb4dd5b1deca8c216
hwansu-k <hs@kumas.dev>
2022-03-13 02:17:25 +0900

    누락파일 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './repositories/user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async findById(id: number, select = false) {
+    const user = await this.userRepository.findById(id, select);
+
+    if (!user) throw new NotFoundException('There is no matching information.');
+
+    return user;
+  }
+}
