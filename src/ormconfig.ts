5151f265f146f685944aacd02b1c878a5836cfa0
zsunkim <jisun124918@gmail.com>
2022-03-29 15:49:19 +0900

    delete entity in typeorm option (only auto)

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -15,12 +15,11 @@ const connectionOptions: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: [User],
+  autoLoadEntities: true,
   synchronize: isDevelopment ? true : false,
   logging: isDevelopment ? true : false,
   migrations: [__dirname + '/src/migrations/*.ts'],
   cli: { migrationsDir: 'src/migrations' },
-  autoLoadEntities: true,
   charset: 'utf8mb4',
 };
 
