e8c863e9cafd7942013efa5879339932f0c487ba
zsunkim <jisun124918@gmail.com>
2022-04-01 00:04:45 +0900

    update meeting module

--- src/meeting/meeting.module.ts
+++ src/meeting/meeting.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Meeting } from './entities/meeting.entity';
 import { MeetingController } from './meeting.controller';
 import { MeetingService } from './meeting.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([]),
+    TypeOrmModule.forFeature([Meeting]),
   ],
   controllers: [MeetingController],
   providers: [MeetingService],
