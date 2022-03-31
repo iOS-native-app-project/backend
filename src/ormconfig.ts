371cbef173214e066e5df03479185cfa92784eca
zsunkim <jisun124918@gmail.com>
2022-03-31 23:23:14 +0900

    entities about meeting in ormconfig ts file

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,9 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import dotenv from 'dotenv';
+import { Category } from './meeting/entities/category.entity';
+import { MeetingUserDetail } from './meeting/entities/meeting-user-detail.entity';
+import { MeetingUser } from './meeting/entities/meeting-user.entity';
+import { Meeting } from './meeting/entities/meeting.entity';
 import { User } from './user/entities/user.entity';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
@@ -15,6 +19,13 @@ const connectionOptions: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  entities: [
+    User,
+    Meeting,
+    Category,
+    MeetingUser,
+    MeetingUserDetail
+  ],
   autoLoadEntities: true,
   synchronize: isDevelopment ? true : false,
   logging: isDevelopment ? true : false,
