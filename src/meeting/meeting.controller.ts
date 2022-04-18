d297db73bfbc3299f7c04999c028d18f8a65ed2f
zsunkim <jisun124918@gmail.com>
2022-04-18 17:29:17 +0900

    update meeting controller

--- src/meeting/meeting.controller.ts
+++ src/meeting/meeting.controller.ts
@@ -1,32 +1,46 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { GetUser } from 'src/auth/auth.decorator';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
+import { User } from 'src/user/entities/user.entity';
 import { MeetingService } from './meeting.service';
 
+@ApiTags('meeting')
 @Controller('meeting')
 export class MeetingController {
   constructor(private readonly meetingService: MeetingService) {}
+  @ApiOperation({ summary: '모임 첫 화면' })
+  // @UseGuards(JwtAuthGuard)
   @Get('/user/:user_id')
-  async getMeeting(@Param('user_id') user_id: number) {
+  async getMeeting(
+    // @GetUser() user: User
+    @Param('user_id') user_id: number,
+  ) {
     return await this.meetingService.getMeeting(user_id);
   }
 
-  @Get('/user/:user_id/category')
+  @ApiOperation({ summary: '카테고리 검색' })
+  @Post('/category')
   async getMeetingByCategory(
+    // @GetUser() user: User,
     @Param('user_id') user_id: number,
     @Body('category_id') category_id: number[],
   ) {
     return await this.meetingService.getMeetingByCategory(category_id, user_id);
   }
 
-  @Get('/user/:user_id/search/:search')
+  @ApiOperation({ summary: '검색어 검색' })
+  @Get('/:user_id/search/:search')
   async getMeetingBySearch(
+    // @GetUser() user: User,
     @Param('user_id') user_id: number,
     @Param('search') search: string,
   ) {
     return await this.meetingService.getMeetingBySearch(search, user_id);
   }
 
-  @Get('/user/:user_id/own')
-  async getMeetingByUserId(@Param('user_id') user_id: number) {
-    return await this.meetingService.getMeetingByUserId(user_id);
+  @Get('/:meeting_id')
+  async getMeetingById(@Param('meeting_id') meeting_id: number) {
+    return await this.meetingService.getMeetingById(meeting_id);
   }
 }
