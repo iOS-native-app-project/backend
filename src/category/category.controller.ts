424d1e6747eafb5b97942ce9d6c13d324e3b83b1
zsunkim <jisun124918@gmail.com>
2022-04-22 14:59:31 +0900

    category 전체 조회 api 생성

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CategoryService } from './category.service';
 
+@ApiTags('Category')
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @ApiOperation({ summary: '모든 카테고리 출력' })
+  @Get()
+  async getCategory() {
+    return await this.categoryService.getCategory();
+  }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryRepository } from './repositories/category.repository';
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+  constructor(
+    @InjectRepository(CategoryRepository)
+    private categoryRepository: CategoryRepository,
+  ) {}
+
+  async getCategory() {
+    return await this.categoryRepository.findAndCount();
+  }
+}
